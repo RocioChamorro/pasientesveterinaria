153b4e2072bd18347a84cf861247c46b0cb5bb8b
RocioChamorro <chamorrorocio5@gmail.com>
2019-11-05 19:23:31 -0500

    Updates

---
+++ precache-manifest.ac0bcf364c704659e019c6c3e1ac4e78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4b588cb289bab3652a5d64db1ee792",
+    "url": "/RocioChamorro/pasientesveterinaria.git/index.html"
+  },
+  {
+    "revision": "f98f7eb38de10bd6c703",
+    "url": "/RocioChamorro/pasientesveterinaria.git/static/css/main.c6cedb9e.chunk.css"
+  },
+  {
+    "revision": "021d8907e11fe87f065f",
+    "url": "/RocioChamorro/pasientesveterinaria.git/static/js/2.b6f5078b.chunk.js"
+  },
+  {
+    "revision": "f98f7eb38de10bd6c703",
+    "url": "/RocioChamorro/pasientesveterinaria.git/static/js/main.e582dab1.chunk.js"
+  },
+  {
+    "revision": "4af64dbf159c3a937dbb",
+    "url": "/RocioChamorro/pasientesveterinaria.git/static/js/runtime-main.a4ec44d3.js"
+  }
+]);
